11dd7e04a13a9a3b514de8e231ab03789ba0857c
Naveen Jain <nveenjain@gmail.com>
2017-11-26 17:45:31 +0530

    button effect added

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@
 document.querySelector('.open1 span').addEventListener('click', function () {
     this.textContent = this.textContent === '>>' ? '<<' : '>>';
     document.querySelector('#left-nav').style.display = document.querySelector('#left-nav').style.display == 'none' ? 'block' : 'none';
+    document.querySelector('canvas').width = document.querySelector('canvas').style.width;
+    console.log(document.querySelector('canvas').width);
 
 })
 document.querySelector('.open2 span').addEventListener('click', function () {
