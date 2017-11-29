b072a931d0c2e4092a9c5de74d41e9cfa3d0ee7a
Naveen Jain <nveenjain@gmail.com>
2017-11-29 17:19:50 +0530

    final commit

--- script/script.js
+++ script/script.js
@@ -21,7 +21,7 @@ window.addEventListener('resize',function resize_canvas() {
     if(dept)
     if(!flag){
         draw_initial();
-    }else check_collision();
+    }else check_collision(pointer);
 });
 
 
@@ -30,19 +30,21 @@ document.querySelector('.open1 span').addEventListener('click', function () {
     this.textContent = this.textContent === '>>' ? '<<' : '>>';
     document.querySelector('#left-nav').style.display = window.getComputedStyle(document.querySelector('#left-nav')).getPropertyValue('display') == 'none' ? 'block' : 'none';
     document.querySelector('canvas').width = parseFloat(window.getComputedStyle(document.querySelector('canvas')).getPropertyValue('width'));
-    if(!flag)
-        draw();
-
+    if(dept){
+        if(!flag)
+            draw();
+    }
 });
 document.querySelector('.open2 span').addEventListener('click', function () {
     this.textContent = this.textContent === '>>' ? '<<' : '>>';
     document.querySelector('#right-nav').style.display = document.querySelector('#right-nav').style.display == 'none' ? 'block' : 'none';
     document.querySelector('canvas').width = parseFloat(window.getComputedStyle(document.querySelector('canvas')).getPropertyValue('width'));
-    if (!flag)
-        draw();
+    if(dept){
+        if (!flag)
+            draw();
     else
-        check_collision();
-
+        check_collision(pointer);
+    }
 
 });
 
@@ -169,13 +171,21 @@ function draw(department, icon){
     let width = canvas.width;
     let height = canvas.height;
     ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'yellow';
     ctx.beginPath();
+    ctx.font = '10px Spectral SC';
+    ctx.strokeText('Event 2',x1-20,y1);
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.strokeStyle="white";
     ctx.arc(x1, y1, 20, 0, 2 * Math.PI);
-    ctx.fill();
+    ctx.stroke();
     ctx.closePath();
     ctx.beginPath();
     ctx.arc(x2, y2, 20, 0, 2 * Math.PI);
-    ctx.fill();
+    ctx.stroke();
+    ctx.strokeStyle = 'yellow';
+    ctx.strokeText('Event 1', x2 - 20, y2);
     ctx.closePath();
     ctx.save();
     ctx.beginPath();
@@ -189,15 +199,16 @@ function draw(department, icon){
     ctx.fillText(`${dept}`, 10, 50);
 }
 //check collision
-async function check_collision(){
+async function check_collision(pointer){
     // let height = canvas.height;
     // let width = canvas.width;
-    let event_data_local, dummy_data;
+    let event_data_local = {}, dummy_data;
     if (dept_avail.indexOf(dept) != -1) {
         //We already loaded department
         event_data_local = event_data[dept_avail.indexOf(dept)];
     } else {
         dummy_data = await fetch(`/data/${dept}.json`).then(d => d.json());
+        event_data_local[dept]= dummy_data;
         dept_avail.push(dept);
         let obj = {};
         obj[dept] = dummy_data;
@@ -208,10 +219,16 @@ async function check_collision(){
     if (Math.sqrt((pointer.x - x2) * (pointer.x - x2) + (pointer.y - y2) * (pointer.y - y2)) < (35)) {
         // ctx.clearRect(0,0,width,height);
         // ctx.fillText(`${event_data_local[dept].event1.title}`, width*10/100 , height*10/100);
-        let modal = document.querySelector('.modal');
-        modal.style.display = "block";
-        modal.style.opacity = "1";
-        modal.querySelector('p').innerHTML = `<h1>${event_data_local[dept].event1.title}</h1> <br /> ${event_data_local[dept].event1.abstract}`;        
+        let $modal = document.querySelector('.modal-frame');
+        let $overlay = document.querySelector('.modal-overlay');
+        $overlay.classList.add('state-show');
+        $modal.classList.remove('state-leave');
+        $modal.classList.add('state-appear');
+        $modal.querySelector('.modal-body').innerHTML = `<h3>${event_data_local[dept].event1.title}</h3>
+                <p>${event_data_local[dept].event1.abstract}
+                    <br /> <b><em>Prize:-</em></b> ${event_data_local[dept].event1.prize}</p>
+                <p class="ps">Co-ordinator:- ${event_data_local[dept].event1.coordinator}
+                <br /> Organisers:- ${event_data_local[dept].event1.organisers}</p>`;        
         // ctx.font = "28px Spectral SC";
         // ctx.fillText(`${event_data_local[dept].event1.abstract}`, width*10/100 , height*10/100 + 60);
         flag = 1;
@@ -220,19 +237,23 @@ async function check_collision(){
     if (Math.sqrt((pointer.x - x1) * (pointer.x - x1) + (pointer.y - y1) * (pointer.y - y1)) < (35)) {
         // ctx.clearRect(0, 0, width, height);
         // ctx.fillText(`${event_data_local[dept].event2.title}`, width * 10 / 100, height * 10 / 100);
-        let modal = document.querySelector('.modal');
-        modal.style.display = "block";
-        modal.style.opacity = "1";
-        modal.querySelector('p').innerHTML = `<h1>${event_data_local[dept].event2.title}</h1> <br /> ${event_data_local[dept].event2.abstract}`;
+        let $modal = document.querySelector('.modal-frame');
+        let $overlay = document.querySelector('.modal-overlay');
+        $overlay.classList.add('state-show');
+        $modal.classList.remove('state-leave');
+        $modal.classList.add('state-appear');
+        $modal.querySelector('.modal-body').innerHTML = `<h3>${event_data_local[dept].event2.title}</h3>
+                <p>${event_data_local[dept].event2.abstract}
+                    <br /> <b><em>Prize:-</em></b> ${event_data_local[dept].event2.prize}</p>
+                <p class="ps">Co-ordinator:- ${event_data_local[dept].event2.coordinator}
+                <br /> Organisers:- ${event_data_local[dept].event2.organisers}</p>`; 
         flag = 1;      
     }
 }
 
 function move(e) {
     if (e.keyCode === 27) {
-        flag = 0;
-        modal.style.opacity = "0";
-        modal.style.display = "none";
+        close_modal();  
     }
     if (!dept || flag)
         return;
@@ -240,21 +261,21 @@ function move(e) {
         case 37:
             if (!flag)
                 (pointer.x > 20) ? pointer.x -= 2 : pointer.x;
-            check_collision();
+            check_collision(pointer);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             draw();
             break;
         case 38:
             if (!flag)
                 (pointer.y > 68) ? pointer.y -= 2 : pointer.y;
-            check_collision();
+            check_collision(pointer);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             draw();
             break;
         case 39:
             if (!flag)
                 (document.querySelector('canvas').width - pointer.x > 20) ? pointer.x += 2 : pointer.x;
-            check_collision();
+            check_collision(pointer);
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             draw();
             break;
@@ -263,7 +284,7 @@ function move(e) {
             if (!flag)
                 (document.querySelector('canvas').height - pointer.y > 20) ? pointer.y += 2 : pointer.y;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            check_collision();
+            check_collision(pointer);
             //check for flag
             draw();
             break;
@@ -283,18 +304,32 @@ span.onclick = function () {
 }
 
 console.log(canvas.getBoundingClientRect());
-canvas.addEventListener('focus',function(e){
+canvas.addEventListener('click',function(e){
     let x = (e.clientX - canvas.getBoundingClientRect().x), y = (e.clientY - canvas.getBoundingClientRect().y);
+    check_collision({x,y});
     console.log(x-pointer.x);
-    if(x - pointer.x >0){
-            move({"keyCode":39});
-    }else{
-            move({"keyCode":37});
-    }
-
-    if(y - pointer.y > 0){
-            move({"keyCode":40});
-    }else{
-            move({"keyCode":38});
-    }
+    
 })
+
+//code for modal starts here 
+
+
+    let $modal = document.querySelector('.modal-frame');
+    let $overlay = document.querySelector('.modal-overlay');
+
+    /* Need this to clear out the keyframe classes so they dont clash with each other between ener/leave. Cheers. */
+    $modal.addEventListener('webkitAnimationEnd oanimationend msAnimationEnd animationend', function (e) {
+        if ($modal.classList.contains('state-leave')) {
+            $modal.classList.remove('state-leave');
+        }
+    });
+function close_modal(){
+        $overlay.classList.remove('state-show');
+        $modal.classList.remove('state-appear');
+        $modal.classList.remove('state-leave');
+        flag = 0;
+}
+document.querySelector('.close').addEventListener('click', close_modal);
+    
+    // document.querySelector('.open').addEventListener('click', function () {
+    // });
